f5bde6785bff73cf868edc28833af20eed818f37
JoseMenta <jrmenta2@gmail.com>
2023-06-03 16:12:22 -0300

    add basic support for selector

--- Makefile
+++ Makefile
@@ -6,11 +6,13 @@ server:
 	cd $(SERVER_DIR); make all
 	mkdir -p $(TARGET_DIR)
 	cp $(SERVER_DIR)/$(SERVER_NAME) $(TARGET_DIR)/$(SERVER_NAME)
+	rm -f $(SERVER_DIR)/$(SERVER_NAME) 
 
 admin:
 	cd $(ADMIN_DIR); make all
 	mkdir -p $(TARGET_DIR)
 	cp $(ADMIN_DIR)/$(ADMIN_NAME) $(TARGET_DIR)/$(ADMIN_NAME)
+	rm -f $(ADMIN_DIR)/$(ADMIN_NAME)
 
 clean:
 	rm -rf $(TARGET_DIR)
--- server/main.c
+++ server/main.c
@@ -1,5 +1,171 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <signal.h>
+
+#include <unistd.h>
+#include <sys/types.h>   // socket
+#include <sys/socket.h>  // socket
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include "selector.h"
+
+
+#define MAX_PENDING_CONNECTIONS 20
+#define INITIAL_FDS 1024
+
+static bool done = false;
+
+static void
+sigterm_handler(const int signal) {
+    printf("signal %d, cleaning up and exiting\n",signal);
+    done = true;
+}
+
 
 int main(int argc, const char* argv[]){
-    printf("Hello Server!\n");
+    unsigned port = 110;
+
+    //No tenemos nada que leer de entrada estandar
+    close(STDIN_FILENO);
+
+    //Variables necesarias para usar el selector
+    const char       *err_msg = NULL;
+    selector_status   ss      = SELECTOR_SUCCESS;
+    fd_selector selector      = NULL; //el selector que usa el servidor
+
+    //Address para hacer el bind del socket
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(port);
+
+    //AF_INET -> indica que usa IPV4
+    //SOCK_STREAM -> flujo multidireccional de datos confiable
+    //IPPROTO_TCP -> indica que va a usar TCP para lograr lo anterior
+    //Devuelve el fd del socket (pasivo)
+    const int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    //Si hubo algun error al abrir el socket
+    if(server < 0) {
+        err_msg = "unable to create socket";
+        goto finally;
+    }
+
+    fprintf(stdout, "Listening on TCP port %d\n", port);
+
+    //SOL_SOCKET -> queremos cambiar propiedades del socket
+    //SO_REUSEADDR -> deja que otro use el puerto inmediatamente cuando deja de usarlo (sirve por si se reinicia)
+    //con el 1, lo mandamos como "habilitado" (recibe un array de opciones)
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+
+    //asigna la direccion al fd server
+    //si retorna negativo falla
+    if(bind(server, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        err_msg = "unable to bind socket";
+        goto finally;
+    }
+
+    //Marca al socket server como un socket pasivo
+    //Si hay mas de MAX_PENDING_CONNECTIONS conexiones en la lista de espera, va a empezar a rechazar algunas 
+    if (listen(server, MAX_PENDING_CONNECTIONS) < 0) {
+        err_msg = "unable to listen";
+        goto finally;
+    }
+
+    //Lista la configuracion del socket, ahora pasamos a la configuracion del selector
+    
+    //Registramos handlers para terminar normalmente en caso de una signal
+    signal(SIGTERM, sigterm_handler);
+    signal(SIGINT,  sigterm_handler);
+    
+    //agrega el flag de O_NONBLOCK a los flags del server para que usarlo sea no bloqueante 
+    if(selector_fd_set_nio(server) == -1) {
+        err_msg = "getting server socket flags";
+        goto finally;
+    }
+
+    //Opciones de configuracion del selector 
+    //Decimos que usamos sigalarm para los trabajos bloqueantes (no nos interesa)
+    //Espera 10 segundos hasta salir del select interno
+    const struct selector_init conf = {
+        .signal = SIGALRM,
+        .select_timeout = {
+            .tv_sec  = 10,
+            .tv_nsec = 0,
+        },
+    };
+
+    //Guarda las configuraciones para el selector
+    if(0 != selector_init(&conf)) {
+        err_msg = "initializing selector";
+        goto finally;
+    }
+
+    //usando las configuraciones del init, crea un nuevo selector con capacidad para 1024 FD's inicialmente
+    selector = selector_new(INITIAL_FDS);
+    if(selector == NULL) {
+        err_msg = "unable to create selector";
+        goto finally;
+    }
+
+    //TODO: cambiar para nuestros handlers
+    const struct fd_handler socksv5 = {
+        .handle_read       = NULL,
+        .handle_write      = NULL,
+        .handle_close      = NULL, // nada que liberar
+    };
+
+    //Registra al fd del server, suscribiendolo para la lectura
+    //Como no necesita un dato auxiliar para los handlers, pasa NULL
+    ss = selector_register(selector, server, &socksv5,
+                                              OP_READ, NULL);
+    if(ss != SELECTOR_SUCCESS) {
+        err_msg = "registering fd";
+        goto finally;
+    }
+
+    for(;!done;) {
+        err_msg = NULL;
+        ss = selector_select(selector);
+        if(ss != SELECTOR_SUCCESS) {
+            //Hubo un error mientras funcionaba el selector
+            err_msg = "serving";
+            goto finally;
+        }
+    }
+
+    //Si llegamos hasta aca sin errores, solo hay que decir que termina el servidor
+    if(err_msg == NULL) {
+        err_msg = "closing";
+    }
+
+    //el valor de retorno del programa
+    int ret = 0;
+    
+    finally:
+    if(ss != SELECTOR_SUCCESS) { //si terminamos con un error del selector
+        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "": err_msg,
+                                  ss == SELECTOR_IO
+                                      ? strerror(errno)
+                                      : selector_error(ss));
+        ret = 2;
+    } else if(err_msg) { //si tuvimos un error anterior
+        perror(err_msg);
+        ret = 1;
+    }
+    if(selector != NULL) { //si pudimos obtener el selector, lo liberamos
+        selector_destroy(selector);
+    }
+    selector_close();
+
+    //Si pudimos obtener el socket, lo cerramos
+    if(server >= 0) {
+        close(server);
+    }
+
+    return ret;
 }

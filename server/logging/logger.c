954697a3eba051cd078d5a5f4ed1d1b20f151d3c
JoseMenta <jrmenta2@gmail.com>
2023-06-16 14:23:23 -0300

    add comments to logger

--- server/logging/logger.c
+++ server/logging/logger.c
@@ -13,13 +13,13 @@
 #define DEFAULT_LOG_FILE_MAXSTRLEN 48
 
 /* Tamaño minimo para el buffer */
-#define LOG_MIN_BUFFER_SIZE 0x1000 // 4 KBs
+#define LOGGER_MIN_BUFFER_SIZE 4096 // 4 KBs
 /* Tamaño maximo para el buffer */
-#define LOG_MAX_BUFFER_SIZE 0x400000 // 4 MBs
+#define LOGGER_MAX_BUFFER_SIZE (LOGGER_MIN_BUFFER_SIZE * 1024) // 4 MBs
 /* De a cuanto se agranda el buffer */
-#define LOG_BUFFER_SIZE_CHUNK 0x1000 // 4 KBs
+#define LOGGER_BUFFER_CHUNK LOGGER_MIN_BUFFER_SIZE // 4 KBs
 /* Tamaño maximo que un unico log deberia requerir */
-#define LOG_BUFFER_MAX_PRINT_LENGTH 0x200 // 512 bytes
+#define LOGGER_BUFFER_MAX_PRINT_LENGTH 512 // 512 bytes
 
 /* 0666 para darle permisos en la creación del archivo */
 #define LOG_FILE_PERMISSION_BITS 0666
@@ -27,6 +27,9 @@
 #define LOG_FOLDER_PERMISSION_BITS 0777
 #define LOG_FILE_OPEN_FLAGS (O_WRONLY | O_APPEND | O_CREAT | O_NONBLOCK)
 
+/*
+ * Obtiene el string representando el nivel del log
+ */
 const char* logger_get_level_string(log_level_t level) {
     switch (level) {
         case LOG_DEBUG:
@@ -46,12 +49,24 @@ const char* logger_get_level_string(log_level_t level) {
 
 #ifndef DISABLE_LOGGER
 
-
+/*
+ * Usamos un buffer variable para no tener siempre el maximo de 4MB
+ * Dejamos variables globales para no tener que pasar siempre un ADT
+ * Vamos a mover memoria solo cuando no tengamos espacio
+ * pero no lo vamos a achicar a medida que sube, porque si necesito mucha
+ * memoria en algun momento, es raro que necesite menos luego
+ */
 static char* buffer = NULL;
+/*
+ * buffer_start: desde donde leer en el buffer
+ * buffer_length: la longitud de lo almacenado en el buffer (se usa para ver desde donde escribir)
+ * buffer_capacity: capacidad disponible del buffer
+ */
 static size_t buffer_start = 0, buffer_length = 0, buffer_capacity = 0;
 
 /* File descriptor para el archivo a escribir */
 static int log_file_fd = -1;
+/* Selector que usa el logger para escribir al archivo sin bloquearse*/
 static fd_selector selector = NULL;
 static log_level_t log_level = MIN_LOG_LEVEL;
 
@@ -59,21 +74,34 @@ static log_level_t log_level = MIN_LOG_LEVEL;
 static FILE* log_stream = NULL;
 
 static void make_buffer_space(size_t len) {
+    //Necesito mas espacio
     if (buffer_length + buffer_start + len > buffer_capacity) {
+        // Para contar lo que esta disponible despues de lo ultimo escrito
+        // buffer_capacity - buffer_length - buffer_start
+        // Si a eso le sumo lo que hay antes de buffer_start
+        // buffer_start
+        // -> buffer_capacity - buffer_length - buffer_start  + buffer_start = buffer_capacity - buffer_length
+        //TODO: no me da esta cuenta, deberia restarle buffer_length
         if (buffer_capacity <= len) {
+            //puedo solucionarlo moviendo los datos al principio de la memoria
             memmove(buffer, buffer + buffer_start, buffer_length);
             buffer_start = 0;
-        } else if (buffer_capacity < LOG_MAX_BUFFER_SIZE) {
+        } else if (buffer_capacity < LOGGER_MAX_BUFFER_SIZE) {
+            //puedo agrandarlo
             size_t new_buffer_capacity = buffer_length + len;
-            new_buffer_capacity = (new_buffer_capacity + LOG_BUFFER_SIZE_CHUNK - 1) / LOG_BUFFER_SIZE_CHUNK * LOG_BUFFER_SIZE_CHUNK;
-            if (new_buffer_capacity > LOG_MAX_BUFFER_SIZE)
-                new_buffer_capacity = LOG_MAX_BUFFER_SIZE;
-
+            //Dejo a la capacidad como multiplos de LOGGER_BUFFER_CHUNK
+            new_buffer_capacity = ((new_buffer_capacity + LOGGER_BUFFER_CHUNK - 1) / LOGGER_BUFFER_CHUNK) * LOGGER_BUFFER_CHUNK;
+            //Si me paso del maximo, vuelvo al maximo
+            if (new_buffer_capacity > LOGGER_MAX_BUFFER_SIZE)
+                new_buffer_capacity = LOGGER_MAX_BUFFER_SIZE;
+            //Reservo la memoria para en nuevo buffer
             void* new_buffer = malloc(new_buffer_capacity);
             if (new_buffer == NULL) {
+                //No pude reservar memoria, entonces dejo lo maximo posible (que no va a ser len)
                 memmove(buffer, buffer + buffer_start, buffer_length);
                 buffer_start = 0;
             } else {
+                //copio en contenido (aprovecho y lo hago al principio)
                 memcpy(new_buffer, buffer + buffer_start, buffer_length);
                 free(buffer);
                 buffer = new_buffer;
@@ -85,30 +113,39 @@ static void make_buffer_space(size_t len) {
 }
 
 /*
- Intenta enviar todo lo que puede al archivo de loggeo
+ Intenta enviar lo que puede al archivo
  Si quedan bytes a escribir le avisamos al selector
  */
-static void try_flush_buffer_to_file() {
+static void try_flush_buffer_to_file(void) {
+    //intenta escribir al archivo de manera no bloqueante (pasamos el flag en open)
     ssize_t written = write(log_file_fd, buffer + buffer_start, buffer_length);
+    //si escribimos algo, avanzamos el buffer
     if (written > 0) {
-        buffer_length -= written;
+        buffer_length -= written; //dejamos de contar lo que escribimos
+        //si lei lo que quedaba en el buffer (buffer_length == 0) me muevo al inicio. Si no, avanzo lo que lei (buffer_start + written)
         buffer_start = (buffer_length == 0 ? 0 : (buffer_start + written));
     }
 
-    // Si quedan para escribir, me sigo interesando en escribir
+    // Si quedan para escribir, me suscribo al fd para escritura (OP_WRITE)
+    // Si no, no me susctibo (OP_NOOP)
     selector_set_interest(selector, log_file_fd, buffer_length > 0 ? OP_WRITE : OP_NOOP);
 }
 
+/*
+ * Funcion llamada por el selector cuando puede escribir en el archivo
+ */
 static void fd_write_handler(struct selector_key* key) {
     try_flush_buffer_to_file();
 }
 
+/*
+ * Funcion llamada por el selector cuando se hace unregister del fd del archivo
+ */
 static void fd_close_handler(struct selector_key* key) {
-    // Intento enviar lo que queda
-
+    // Me quedaron cosas por escribir
     if (buffer_length != 0) {
-        // Lo hago bloqueante para que termine, TODO fijarse si esta bien
         int flags = fcntl(log_file_fd, F_GETFD, 0);
+        //Lo dejamos de hacer no bloqueante, no nos preocupa quedarnos bloqueados para mandar lo que falta
         fcntl(log_file_fd, F_SETFL, flags & (~O_NONBLOCK));
         ssize_t written = write(log_file_fd, buffer, buffer_length);
         if (written > 0) {
@@ -116,8 +153,9 @@ static void fd_close_handler(struct selector_key* key) {
             buffer_start = (buffer_length == 0 ? 0 : (buffer_start + written));
         }
     }
-
+    //cerramos el fd al archivo
     close(log_file_fd);
+    //por si lo vuelven a usar (no va a pasar)
     log_file_fd = -1;
 }
 
@@ -132,7 +170,7 @@ static int try_open_log_file(const char* log_file, struct tm tm) {
     if (log_file == NULL)
         return -1;
 
-    char log_file_buffer[DEFAULT_LOG_FILE_MAXSTRLEN + 1];
+    char log_file_buffer[DEFAULT_LOG_FILE_MAXSTRLEN + 1] = {0};
 
     // Si es "", le ponemos un default (YYYY-MM-DD.log), si ya existe se sigue loggeando ahi
     if (log_file[0] == '\0') {
@@ -166,6 +204,7 @@ int logger_init(fd_selector selector_param, const char* log_file, FILE* log_stre
     struct tm tm = *localtime(&timeNow);
 
     selector = selector_param;
+    //Si no me pasan el selector, no voy a poder usar un archivo porque no me quiero bloquear
     log_file_fd = selector_param == NULL ? -1 : try_open_log_file(log_file, tm);
     log_stream = log_stream_param;
     log_level = MAX_LOG_LEVEL;
@@ -177,8 +216,8 @@ int logger_init(fd_selector selector_param, const char* log_file, FILE* log_stre
 
     // Chequeo que estoy loggeando por archivo o por Stream
     if (log_file_fd >= 0 || log_stream != NULL) {
-        buffer = malloc(LOG_MIN_BUFFER_SIZE);
-        buffer_capacity = LOG_MIN_BUFFER_SIZE;
+        buffer = malloc(LOGGER_MIN_BUFFER_SIZE);
+        buffer_capacity = LOGGER_MIN_BUFFER_SIZE; //tamaño total del buffer (memoria reservada)
         buffer_length = 0;
         buffer_start = 0;
         if (buffer == NULL) {
@@ -191,13 +230,15 @@ int logger_init(fd_selector selector_param, const char* log_file, FILE* log_stre
     return 0;
 }
 
-int logger_finalize() {
+int logger_finalize(void) {
+    //tenemos el archivo, lo sacamos del selector
     if (log_file_fd >= 0) {
         selector_unregister_fd(selector, log_file_fd);
         selector = NULL;
     }
 
     if (buffer != NULL) {
+        //liberamos el buffer que usa el logger
         free(buffer);
         buffer = NULL;
         buffer_capacity = 0;
@@ -217,12 +258,15 @@ int logger_is_enabled_for(log_level_t level) {
     return level >= log_level && (log_file_fd > 0 || log_stream != NULL);
 }
 
-void logger_pre_print() {
-    make_buffer_space(LOG_BUFFER_MAX_PRINT_LENGTH);
+void logger_pre_print(void) {
+    //nos aseguramos de que en el buffer tengamos
+    make_buffer_space(LOGGER_BUFFER_MAX_PRINT_LENGTH);
 }
 
 void logger_get_bufstart_and_maxlength(char** bufstartVar, size_t* maxlenVar) {
+    //desde donde puede escribir
     *bufstartVar = buffer + buffer_start + buffer_length;
+    //cuanto puede escribir (si no hay suficiente espacio porque se paso del maximo, se va a perder el log)
     *maxlenVar = buffer_capacity - buffer_length - buffer_start;
 }
 
@@ -237,14 +281,14 @@ int logger_post_print(int written, size_t maxlen) {
         fprintf(stderr, "Error: %lu bytes of logs possibly lost\n", written - maxlen + 1);
         written = maxlen - 1;
     }
-
+    //imprimimos tambien por el stream si lo pasaron
     if (log_stream != NULL) {
         fprintf(log_stream, "%s", buffer + buffer_start + buffer_length);
     }
 
     if (log_file_fd >= 0) {
-        buffer_length += written;
-        try_flush_buffer_to_file();
+        buffer_length += written; //actualizo buffer_length para reflejar lo que se escribio
+        try_flush_buffer_to_file(); //intento mandarlo al archivo
     }
     return 0;
 }
